aa768c1828b438ee8b845e191b682f4f989d6064
Rock <rock.zhang.cn@gmail.com>
2015-03-11 22:18:14 +0800

    精简代码，重命名变量文件等

--- JniTest/jni/Android.mk
+++ JniTest/jni/Android.mk
@@ -8,19 +8,16 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/un7z/src/
 
 LOCAL_MODULE := un7z
 
-LOCAL_CFLAGS := -DDEBUG 
-				
 LOCAL_SRC_FILES := \
 		src/xz_crc32.cpp \
 		src/xz_crc64.cpp \
 		src/xz_dec_bcj.cpp \
 		src/xz_dec_lzma2.cpp \
 		src/xz_dec_stream.cpp \
-		src/xzminidec.cpp \
 		JniLoad.cpp
         
-LOCAL_CFLAGS := -O2 -g -DTENCENT  -DXZ_DEC_X86 -DXZ_DEC_POWERPC -DXZ_DEC_IA64 \
-		-DXZ_DEC_ARM -DXZ_DEC_ARMTHUMB -DXZ_DEC_SPARC  -DXZ_USE_CRC64 -DXZ_DEC_ANY_CHECK 
+LOCAL_CFLAGS := -DXZ_DEC_X86 -DXZ_DEC_POWERPC -DXZ_DEC_IA64 -DXZ_DEC_ARM -DXZ_DEC_ARMTHUMB \
+				-DXZ_DEC_SPARC  -DXZ_USE_CRC64 -DXZ_DEC_ANY_CHECK 
 
 LOCAL_LDLIBS := -llog 
 
